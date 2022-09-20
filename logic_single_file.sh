d36a848a4bf45ea2b2e1112355a34652fce5db47
joelsare <joelsare@gmail.com>
2022-09-20 14:38:22 -0500

    update logic scripts

--- logic_single_file.sh
+++ logic_single_file.sh
@@ -2,13 +2,11 @@
 . config.txt
 
 echo "Text2ALM..."
-echo $2
-mkdir $logicFileLocation/$2
 python3 CommandCenter.py text2alm --input $1
 echo "${1##*/}"
 fbname=$(basename "$1" .txt)
 echo "$fbname"
 echo "Sparc..."
 rm $logicFileLocation/$fbname$".tp.lp"
-java -jar sparc.jar Output/Text2ALM_Outputs/$fbname/CALM/$fbname$".tp.sparc" -A --disable-empty-sort-check -o $logicFileLocation/$2/$fbname$".tp.lp" >& MessagesFromSPARC
+java -jar sparc.jar Output/Text2ALM_Outputs/$fbname/CALM/$fbname$".tp.sparc" -A --disable-empty-sort-check -o $logicFileLocation/$fbname$".tp.lp" >& MessagesFromSPARC
 
